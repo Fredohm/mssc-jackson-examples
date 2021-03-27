6b6087d1963047138852b90a2f676f6b04a6ac8c
Fredohm <fredwaucquez@hotmail.fr>
2021-03-27 14:37:35 +0100

    105. Json Testing with Spring Boot

---
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -0,0 +1,36 @@
+package guru.springframework.msscjacksonexamples.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@JsonTest
+class BeerDtoTest extends BaseTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void testSerializeDto() throws JsonProcessingException {
+        BeerDto beerDto = getDto();
+
+        String jsonString = objectMapper.writeValueAsString(beerDto);
+
+        System.out.println(jsonString);
+    }
+
+    @Test
+    void testDeserializeDto() throws IOException {
+        String json = "{\"id\":\"dac367d9-fe7d-4c4e-babf-a73bd3f32df3\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123,\"price\":12.99,\"createdDate\":\"2021-03-27T14:35:06.5421654+01:00\",\"lastUpdatedDate\":\"2021-03-27T14:35:06.5441693+01:00\"}";
+
+        BeerDto beerDto = objectMapper.readValue(json, BeerDto.class);
+
+        System.out.println(beerDto);
+    }
+}
